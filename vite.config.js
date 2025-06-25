a9f381aa45ed8c5ca51a0ce88c6008483b635a8b
ignacio bortolin <ignaciowebcontact@gmail.com>
2025-06-25 15:41:56 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/browser-extensions-manager-ui/',  // <- debe coincidir con el nombre de tu repo
   plugins: [react()],
-  base: 'browser-extension-manager-ui/',
-})
+});
